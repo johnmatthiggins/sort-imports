9851e40a55a52ad9c687eaa71e84485d6ae8b8f4
John Higgins <johnmatthiggins@gmail.com>
2024-03-23 11:22:20 -0700

    wrote import sorter

---
+++ app.go
@@ -0,0 +1,85 @@
+package main
+
+// Author: johnmatthiggins@gmail.com
+// Program: JSX Import Sorter
+//
+// A program that sorts JSX imports. It can be invoked by
+// hitting "!" and typing the name of the binary after
+// selecting some text in visual mode.
+
+import (
+	"bufio"
+	"cmp"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+type ImportStatement struct {
+	componentName string
+	importPath    string
+	rawLine       string
+}
+
+func main() {
+	var lines []string
+
+	// Read in lines until you get to EOT character
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "." {
+			break
+		}
+		lines = append(lines, line)
+	}
+
+	statements := make([]ImportStatement, len(lines))
+
+	for i := 0; i < len(statements); i++ {
+		importStatement, err := parseImportStatement(lines[i])
+		if err != nil {
+			// print out original text if we can't figure out
+			// how to parse it...
+			fmt.Print(strings.Join(lines, "\n"))
+			return
+		}
+		statements[i] = *importStatement
+	}
+
+	slices.SortFunc(statements, compareImportStatements)
+	for _, statement := range statements {
+		statement.PrintStatement()
+	}
+}
+
+func compareImportStatements(a, b ImportStatement) int {
+	result := cmp.Compare(a.importPath, b.importPath)
+	if result == 0 {
+		result = cmp.Compare(a.componentName, b.componentName)
+	}
+	return result
+}
+
+func (stmt *ImportStatement) PrintStatement() {
+	fmt.Printf("import %s from %s\n", stmt.componentName, stmt.importPath)
+}
+
+func parseImportStatement(line string) (*ImportStatement, error) {
+	segments := strings.Split(line, " ")
+	if len(segments) != 4 {
+		err := fmt.Errorf(
+			"Expected 4 segments after splitting on space character, but found %d.",
+			len(segments),
+		)
+		return nil, err
+	}
+	importStatement := ImportStatement{
+		componentName: segments[1],
+		importPath:    segments[3],
+		rawLine:       line,
+	}
+
+	return &importStatement, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module johnmatthiggins/sort-imports
+
+go 1.22.0
